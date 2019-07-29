975c496006f5330e41f634a8a698e89dcb08857c
Alain Kaeslin <ak@alainkaeslin.com>
2019-07-29 19:50:59 +0200

    Add README.md.

--- web/src/components/About.js
+++ web/src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const About = () => (
     <div>
         <h2>About</h2>
-        This webpage is a just for fun project. The data is obtained from <a href="https://timetable.search.ch/api/help">search.ch</a>.
+        This is a just for fun project. The data is obtained from <a href="https://timetable.search.ch/api/help">search.ch</a>.
         <br/>
-        All information here is possibly incomplete or wrong due to errors in the input data or bugs in the postprocessing performed by this page.
+        You can contribute at <a href="https://github.com/alakae/trackz">github.com/alakae/trackz</a>.
     </div>
 );
