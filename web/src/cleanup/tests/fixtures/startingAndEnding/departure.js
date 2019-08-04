9be15081ff149705ec32aeaeae2c66c633875959
Alain Kaeslin <ak@alainkaeslin.com>
2019-08-04 17:05:21 +0200

    Add turn test.

---
+++ web/src/cleanup/tests/fixtures/startingAndEnding/departure.js
@@ -0,0 +1,18 @@
+export const departureStartingAndEnding = {
+    "stop": {"id": "8503125", "name": "Uster", "x": "696717", "y": "245178"},
+    "connections": [{
+        "time": "2019-06-29 19:40:00",
+        "*G": "S",
+        "*L": "9",
+        "type": "strain",
+        "line": "S9",
+        "operator": "SBB",
+        "number": "S9 18976",
+        "color": "039~fff~",
+        "type_name": "S-Train",
+        "terminal": {"id": "8503424", "name": "Schaffhausen", "x": 689634, "y": 283756},
+        "track": "3"
+    },],
+    "request": "stationboard",
+    "eof": 1
+};
