9261c9b1ba51d9b1537a73c84e141b3bd781a205
Alain Kaeslin <ak@alainkaeslin.com>
2019-08-04 17:38:34 +0200

    Add failing test for Spiez.

---
+++ web/src/cleanup/tests/fixtures/fluegelungAt/departure.js
@@ -0,0 +1,28 @@
+export const departureFluegelungAt = {
+    "stop": {"id": "8507483", "name": "Spiez", "x": "618472", "y": "170603"},
+    "connections": [{
+        "time": "2019-08-04 18:12:00",
+        "*G": "RE",
+        "type": "express_train",
+        "line": "RE",
+        "operator": "BLS-bls",
+        "number": "RE 4281",
+        "color": "f00~fff~",
+        "type_name": "Railway",
+        "terminal": {"id": "8301003", "name": "Domodossola (I)", "x": 666300, "y": 107450},
+        "track": "3CD"
+    }, {
+        "time": "2019-08-04 18:12:00",
+        "*G": "R",
+        "type": "train",
+        "line": "R",
+        "operator": "BLS-bls",
+        "number": "R 6833",
+        "color": "039~fff~",
+        "type_name": "Railway",
+        "terminal": {"id": "8507290", "name": "Zweisimmen", "x": 595144, "y": 155758},
+        "track": "3AB"
+    }],
+    "request": "stationboard",
+    "eof": 1
+};
