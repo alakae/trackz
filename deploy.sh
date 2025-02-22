ab64f0b903a78568fef1f9298a3736b30acc98b9
Alain Kaeslin <ak@alainkaeslin.com>
2025-02-22 15:28:51 +0100

    Start rewrite.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-docker build -t alakae/trackz:latest -t alakae/trackz:$SHA -f ./web/Dockerfile ./web
-
-docker push alakae/trackz:latest
-
-docker push alakae/trackz:$SHA
-
-kubectl apply -f k8s
-kubectl -n trackz set image deployments/web-deployment web=alakae/trackz:$SHA
--- web/src/cleanup/tests/fixtures/starting/arrival.js
+++
@@ -1,6 +0,0 @@
-export const arrivalStarting = {
-    "stop": {"id": "8503000", "name": "Zürich HB", "x": "683212", "y": "248030"},
-    "connections": [],
-    "request": "stationboard",
-    "eof": 1
-};
