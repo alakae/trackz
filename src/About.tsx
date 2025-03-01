49d224bda44a645048ecdba4c7e7a9d4fc0d040c
Alain Kaeslin <ak@alainkaeslin.com>
2025-03-01 11:22:40 +0100

    Update About page content with project info and links

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,10 @@
 export const About = () => (
   <div className="page-container">
     <h1>About Us</h1>
-    <p>Learn more about our mission and story</p>
+    This is a just for fun project. The data is obtained from{" "}
+    <a href="https://timetable.search.ch/api/help">search.ch</a>.
+    <br />
+    You can contribute at{" "}
+    <a href="https://github.com/alakae/trackz">github.com/alakae/trackz</a>.
   </div>
 );
