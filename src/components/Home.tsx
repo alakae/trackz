ddd0b6d9ee29071fea72581f21f87094e3941f34
Alain Kaeslin <ak@alainkaeslin.com>
2025-12-13 00:39:26 +0100

    docs(Home): update description text for improved clarity.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,9 @@
 export const Home = () => (
   <div className="page-container">
     <h1>Welcome to Trackz</h1>
-    <p>Discover your next favorite station</p>
+    <p>
+      The graphical arrival and departure board for railway stations in
+      Switzerland.
+    </p>
   </div>
 );
